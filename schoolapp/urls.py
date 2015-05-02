d2371733be69f421a752f9955e5a17bdb28f93bd
Sergey Samoshkin <sergey.samoshkin@gmail.com>
2015-05-02 16:08:55 +0300

    django generic views

--- schoolapp/urls.py
+++ schoolapp/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'(?P<school_id>[0-9]+)/classes/$', views.classes, name='classes'),
-    url(r'(?P<school_id>[0-9]+)/class/(?P<class_id>[0-9]+)/$', views.schoolclass, name='schoolclass'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'(?P<pk>[0-9]+)/classes/$', views.ClassList.as_view(), name='classes'),
+    url(r'(?P<school_id>[0-9]+)/class/(?P<pk>[0-9]+)/$', views.SchoolClassView.as_view(), name='schoolclass'),
     url(r'(?P<school_id>[0-9]+)/class/(?P<class_id>[0-9]+)/add_hw$', views.add_hw, name='add_hw'),
-    url(r'(?P<school_id>[0-9]+)/$', views.school, name='school'),
+    url(r'(?P<pk>[0-9]+)/$', views.SchoolView.as_view(), name='school'),
 ]
--- schoolapp/views.py
+++ schoolapp/views.py
@@ -1,38 +1,45 @@
 # Create your views here.
 from django.shortcuts import render, redirect, get_object_or_404
-from django.template import RequestContext
+from django.views import generic
 
 from .models import School, SchoolClass
 
 
-def index(request):
-    obj_list = School.objects.all()
-    if len(obj_list) == 1:
-        return redirect('school:school', school_id=obj_list[0].id)
-    return render(request, 'school/list.html', RequestContext(request, {
-        'school_list': obj_list
-    }))
+class IndexView(generic.ListView):
+    def get_queryset(self):
+        return School.objects.all()
 
 
-def classes(request, school_id):
-    obj = get_object_or_404(School, pk=school_id)
-    class_list = obj.schoolclass_set.all()
-    return render(request, 'school/classes.html', RequestContext(request, {
-        'school': obj, 'class_list': class_list
-    }))
+#def index(request):
+#    obj_list = School.objects.all()
+#     if len(obj_list) == 1:
+#         return redirect('school:school', school_id=obj_list[0].id)
+#     return render(request, 'school/list.html', RequestContext(request, {
+#         'school_list': obj_list
+#     }))
 
 
-def school(request, school_id):
-    obj = get_object_or_404(School, pk=school_id)
-    return render(request, 'school/index.html', { 'school': obj })
+class ClassList(generic.ListView):
+    def get_queryset(self):
+        obj = get_object_or_404(School, pk=self.kwargs['pk'])
+        return obj.schoolclass_set.all()
 
 
-def schoolclass(request, school_id, class_id):
-    obj = get_object_or_404(SchoolClass, pk=class_id, school=school_id)
-    home_works = obj.homework_set.order_by('-created_at')[:20]
-    return render(request, 'school/class.html', RequestContext(request, {
-        'schoolclass': obj, 'school': obj.school, 'home_works': home_works
-    }))
+class SchoolView(generic.DetailView):
+    model = School
+
+
+class SchoolClassView(generic.DetailView):
+    model = SchoolClass
+
+    def get_queryset(self):
+        qs = super(SchoolClassView, self).get_queryset()
+        return qs.filter(school=self.kwargs['school_id'])
+
+    def get_context_data(self, **kwargs):
+        context = super(SchoolClassView, self).get_context_data(**kwargs)
+        context['home_works'] = self.get_object().homework_set.order_by('-created_at')[:20]
+        return context
 
 
 def add_hw(request, school_id, class_id):
@@ -43,3 +50,4 @@ def add_hw(request, school_id, class_id):
         })
     obj.add_home_work(request.POST['description'])
     return redirect('school:schoolclass', obj.school.id, obj.id)
+
